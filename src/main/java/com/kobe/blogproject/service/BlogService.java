1ac3d31d83b09a7baa9f49eabd64e7910b86a520
Kobe <dev.skyachieve91@gmail.com>
2025-06-14 14:45:05 +0900

    feat: findAll() 메서드 생성 및 구현.
    - JPA 지원 메서드인 findAll()을 호출해 article 테이블에 저장되어 있는 모든 데이터를 조회

--- src/main/java/com/kobe/blogproject/service/BlogService.java
+++ src/main/java/com/kobe/blogproject/service/BlogService.java
@@ -6,6 +6,8 @@ import com.kobe.blogproject.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
 @Service // 빈으로 등록
 public class BlogService {
@@ -15,5 +17,10 @@ public class BlogService {
 	// 블로그 글 추가 메서드
 	public Article save(AddArticleRequest request) {
 		return blogRepository.save(request.toEntity());
+	}
+
+	// 블로그 글 가져오기 메서드
+	public List<Article> findAll() {
+		return blogRepository.findAll();
 	}
 }
