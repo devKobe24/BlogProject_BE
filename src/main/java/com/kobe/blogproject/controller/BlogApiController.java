c8b2cce896c2be7ab4e27eefb64812da3ee8b242
Kobe <dev.skyachieve91@gmail.com>
2025-06-14 14:56:32 +0900

    feat: findAllArticles() 메서드 생성 및 구현.
    - 전체 글을 조회한 뒤 반환하는 메서드
    - /api/articles GET 요청이 오면 글 전체를 조회하는 findAll() 메서드를 호출한 다음 응답용 객체인 ArticleResponse로 파싱해 body에 담아 클라이언트에게 전송.
    - 이 코드는 스트림을 적용함.

--- src/main/java/com/kobe/blogproject/controller/BlogApiController.java
+++ src/main/java/com/kobe/blogproject/controller/BlogApiController.java
@@ -2,13 +2,14 @@ package com.kobe.blogproject.controller;
 
 import com.kobe.blogproject.domain.Article;
 import com.kobe.blogproject.dto.request.AddArticleRequest;
+import com.kobe.blogproject.dto.response.ArticleResponse;
 import com.kobe.blogproject.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController // HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
@@ -24,5 +25,16 @@ public class BlogApiController {
 		// 요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 응답 객체에 담아 전송
 		return ResponseEntity.status(HttpStatus.CREATED)
 			.body(savedArticle);
+	}
+
+	@GetMapping("/api/articles")
+	public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+		List<ArticleResponse> articles = blogService.findAll()
+			.stream()
+			.map(ArticleResponse::new)
+			.toList();
+
+		return ResponseEntity.ok()
+			.body(articles);
 	}
 }
