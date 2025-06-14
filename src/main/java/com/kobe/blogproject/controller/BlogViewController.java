f199bf1e942a8ee6a1bba85fd17f043210550fa9
Kobe <dev.skyachieve91@gmail.com>
2025-06-15 08:02:13 +0900

    feat: BlogViewController 생성 및 구현.
    - /articles GET 요청을 처리할 코드를 작성.
    - 여기서는 블로그 글 전체 리스트를 담은 뷰를 반환합니다.

---
+++ src/main/java/com/kobe/blogproject/controller/BlogViewController.java
@@ -0,0 +1,27 @@
+package com.kobe.blogproject.controller;
+
+import com.kobe.blogproject.dto.response.ArticleListViewResponse;
+import com.kobe.blogproject.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+
+	private final BlogService blogService;
+
+	@GetMapping("/articles")
+	public String articles(Model model) {
+		List<ArticleListViewResponse> articles = blogService.findAll().stream()
+			.map(ArticleListViewResponse::new)
+			.toList();
+		model.addAttribute("articles", articles); // 블로그 글 리스트 저장
+
+		return "articleList"; // articleList.html라는 뷰 조회
+	}
+}
