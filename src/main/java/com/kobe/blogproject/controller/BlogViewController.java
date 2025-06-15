c8e64e11695daac705e91d09fdea9ee498d7f149
Kobe <dev.skyachieve91@gmail.com>
2025-06-15 12:18:03 +0900

    feat: getArticle() 메서드 추가.
    - 블로그 글을 반환할 컨트롤러의 메서드 작성.
    - 인자 id에 URL로 넘어온 값을 받아 findById() 메서드로 넘겨 글을 조회하고, 화면에서 사용할 모델에 데이터를 저장한 다음, 보여줄 화면의 템플릿 이름을 반환합니다.

--- src/main/java/com/kobe/blogproject/controller/BlogViewController.java
+++ src/main/java/com/kobe/blogproject/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package com.kobe.blogproject.controller;
 
+import com.kobe.blogproject.domain.Article;
 import com.kobe.blogproject.dto.response.ArticleListViewResponse;
+import com.kobe.blogproject.dto.response.ArticleViewResponse;
 import com.kobe.blogproject.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -23,5 +26,14 @@ public class BlogViewController {
 		model.addAttribute("articles", articles); // 블로그 글 리스트 저장
 
 		return "articleList"; // articleList.html라는 뷰 조회
+	}
+
+	// 블로그 글을 반환하는 메서드
+	@GetMapping("/articles/{id}")
+	public String getArticle(@PathVariable Long id, Model model) {
+		Article article = blogService.findById(id);
+		model.addAttribute("article", new ArticleViewResponse(article));
+
+		return "article";
 	}
 }
