faee5e6d6b2a3ba10b71580f2f07aba9ee4f80e8
Kobe <dev.skyachieve91@gmail.com>
2025-06-15 13:40:35 +0900

    feat: newArticle() 메서드 생성 및 구현.
    - 쿼리 파라미터로 넘어온 id값은 newArticle() 메서드의 Long 타입 id 인자에 매핑합니다.(이 값은 없을 수도 있습니다.)
    - id가 있으면 수정, 없으면 생성이므로 id가 없는 경우 기본 생성자를 이용해 빈 ArticleViewResponse 객체를 만들고, id가 있으면 기존 값을 가져오는 findById() 메서드를 호출합니다.

--- src/main/java/com/kobe/blogproject/controller/BlogViewController.java
+++ src/main/java/com/kobe/blogproject/controller/BlogViewController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -35,5 +36,19 @@ public class BlogViewController {
 		model.addAttribute("article", new ArticleViewResponse(article));
 
 		return "article";
+	}
+
+	// 수정 화면을 보여주기 위한 메서드
+	@GetMapping("/new-article")
+	// id 키를 가진 쿼라 파라미터의 값을 id 변수에 매핑(id는 없을 수도 있음)
+	public String newArticle(@RequestParam(required = false) Long id, Model model) {
+		if (id == null) { // id가 없으면 생성
+			model.addAttribute("article", new ArticleViewResponse());
+		} else {
+			Article article = blogService.findById(id);
+			model.addAttribute("article", new ArticleViewResponse(article));
+		}
+
+		return "newArticle";
 	}
 }
