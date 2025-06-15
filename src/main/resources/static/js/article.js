3fe8a3ffdd5027734bfd7dc6571b425cd94bf4ba
Kobe <dev.skyachieve91@gmail.com>
2025-06-15 12:57:16 +0900

    feat: article.js 생성 및 구현.
    - HTML에서 id를 delete-btn으로 설정한 엘리먼트를 찾아 그 엘리먼트에서 클릭 이벤트가 발생하면 fetch() 메서드에 이어지는 then() 메서드는 fetch()가 잘 완료되면 연이어 실행되는 메서드입니다.
    
    - alert() 메서드는 then() 메서드가 실행되는 시점에 웹 브라우저 화면으로 삭제가 완료되었음을 알리는 팝업을 띄워주는 메서드입니다.
    
    - location.replace() 메서드는 실행 시 사용자의 웹 브라우저 화면을 현재 주소를 기반해 옮겨주는 역할을 합니다.

---
+++ src/main/resources/static/js/article.js
@@ -0,0 +1,15 @@
+// 삭제 기능
+const deleteBtn = document.getElementById('delete-btn');
+
+if (deleteBtn) {
+    deleteBtn.addEventListener('click', event => {
+        let id = document.getElementById('article-id').value;
+        fetch(`/api/articles/${id}`, {
+            method: 'DELETE'
+        })
+            .then(() => {
+                alert('삭제가 완료되었습니다.');
+                location.replace(`/articles`);
+            });
+    });
+}
